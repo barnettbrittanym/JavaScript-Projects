ae787b6e413a94e4a76bafc18d9e94e7e4ca6502
Brittany Barnett <barnett.brittanym@gmail.com>
2021-03-12 20:14:51 -0500

    update

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,21 @@
+document.write ("Lisa told Bart, \"Knock if off, Bart! Or I'll tell dad!\" <br>\"Eat my shorts!\" Bart responded.");
+window.alert("Brittany told Jakob \"No\" ");
+
+document.write ("This is" + " a concantenation");
+
+var A= "This string is" + " concantenated";
+document.write (A);
+
+var B="banana" //assigns three different variables
+var C="pineapple"
+var D="apple"
+
+document.write(D);
+
+var B=B.fontcolor("yellow");//assigns colors to the previously named variables
+var C=C.fontcolor("orange");
+var D=D.fontcolor("red");
+
+document.write(B,C,D); //printing out the names of the variables
+document.write(A,D);
+document.write(3*95); //performs a basic mathematic fuction
