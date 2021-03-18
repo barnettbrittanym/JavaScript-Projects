b10253098f87a6e3e69090cedd105bbe4dca51bf
Brittany Barnett <barnett.brittanym@gmail.com>
2021-03-17 23:02:45 -0400

    Commented

--- Basic JavaScript Projects/Project2_functions/main.js
+++ Basic JavaScript Projects/Project2_functions/main.js
@@ -1,11 +1,11 @@
 function firstFunction(){
     var str= "This text is purple!!"
-    var result= str.fontcolor("purple");
+    var result= str.fontcolor("purple");//Changes the button font color to purple
     document.getElementById("purple_text").innerHTML=result;
 }
 
 function secondFunction(){
     let sentence="I am learning ";
-    sentence+= "a lot in this course!"
+    sentence+= "a lot in this course!"//Here I used the += to concantenate!
     document.getElementById("noodle").innerHTML=sentence;
 }
